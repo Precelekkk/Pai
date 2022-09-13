998895fa53a511b726d6a9053c00077f1f2d69f3
Precelekkk <marcinbizness@gmail.com>
2022-09-13 11:06:55 +0200

    h

---
+++ lekcja 1/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>strona WWWA</title>
+</head>
+<body>
+<center><h2>ICH LEBIE LOS</h2></center>
+<?php
+$zmienna = "ta";
+echo " hello <br>". $zmienna . " <br> halo ";
+?>
+</body>
+</html>
